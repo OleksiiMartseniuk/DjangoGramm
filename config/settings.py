856b76ca5ea4e7b5cf440f4759ac393bdda1b8f5
alex <martsenyk.l@gmail.com>
2022-01-14 23:55:57 +0200

    add .env

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u)(k$95u(94q_dmr7gug0@#du=rqhb5_jnv7re%za+sik3p^$w'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
