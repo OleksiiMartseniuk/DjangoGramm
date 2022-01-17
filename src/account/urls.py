d1c34e422665fe772da91c839e6930ede74cfa3c
alex <martsenyk.l@gmail.com>
2022-01-17 16:32:23 +0200

    create url profile_edit

--- src/account/urls.py
+++ src/account/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 
     path('register/', RegistrationUser.as_view(), name='register'),
     path('register-to-email/', ToRegistrationUserEmail.as_view(), name='register_to_email'),
+    path('register/email/<uidb64>', ProfileEdit.as_view(), name='profile_edit'),
 
-    path('profile/', ProfileEdit.as_view(), name='profile_edit'),
 ]
