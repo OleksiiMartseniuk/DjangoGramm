f847452153ab423eea90c776fca1fc04b93ebbd7
alex <martsenyk.l@gmail.com>
2022-01-18 16:29:17 +0200

    flag switch activ_email

--- src/account/views.py
+++ src/account/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 from .models import CustomUser
-from .forms import UserRegisterForm, ProfileForm
+from .forms import UserRegisterForm, ProfileCreateForm
 from ..base.services import sent_email_register
 
 
@@ -23,8 +23,8 @@ class RegistrationUser(CreateView):
     success_url = reverse_lazy('register_to_email')
 
     def form_valid(self, form):
-        sent_email_register(form.cleaned_data['email'],
-                            form.cleaned_data['username'],
+        sent_email_register(form.instance.email,
+                            form.instance.username,
                             self.request)
         return super().form_valid(form)
 
@@ -34,13 +34,16 @@ class ToRegistrationUserEmail(TemplateView):
 
 
 class ProfileEdit(UpdateView, SuccessMessageMixin):
-    form_class = ProfileForm
+    form_class = ProfileCreateForm
     template_name = 'account/profile/profile_create.html'
     success_url = reverse_lazy('login')
     success_message = 'Data saved'
 
     def get_object(self, queryset=None):
-        self.object.activ_email = True
         username = urlsafe_base64_decode(self.kwargs['uidb64'])
         return CustomUser.objects.get(username=bytes.decode(username, 'utf-8'))
 
+    def form_valid(self, form):
+        if not form.instance.activ_email:
+            form.instance.activ_email = True
+        return super().form_valid(form)
