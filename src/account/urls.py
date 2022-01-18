ca2bf9b768fe7180bc638e60de30ac1762cc06e5
alex <martsenyk.l@gmail.com>
2022-01-18 22:08:25 +0200

    add templates profile and path ('profile')

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from src.account.views import index
+from src.account.views import index, ProfileListView
 
 urlpatterns = [
     path('', index, name='home'),
+    path('profile/<slug:username>/', ProfileListView.as_view(), name='profile')
 ]
