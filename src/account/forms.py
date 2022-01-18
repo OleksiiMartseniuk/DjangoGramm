dd03d4f77151d7d8cccf6bd873d3eb2756728d48
alex <martsenyk.l@gmail.com>
2022-01-18 16:31:39 +0200

    extensions method clear and predestination field username

--- src/account/forms.py
+++ src/account/forms.py
@@ -1,7 +1,29 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from src.account.models import CustomUser
+from src.base.services import get_or_none, sent_email_register
+
+
+class LoginForm(AuthenticationForm):
+
+    def __init__(self, request, *args, **kwargs):
+        super(LoginForm, self).__init__(request=None, *args, **kwargs)
+        self._request = request
+        self.fields['username'].label = 'Username or Email'
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        user = get_or_none(CustomUser, username=username)
+        if user:
+            if not user.activ_email:
+                sent_email_register(email=user.email,
+                                    username=user.username,
+                                    request=self._request)
+                raise ValidationError('An email has been resent to you. Follow the link to complete registration')
+        super(LoginForm, self).clean()
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,7 +32,8 @@ class UserRegisterForm(UserCreationForm):
         fields = ('username', 'email')
 
 
-class ProfileForm(forms.ModelForm):
+class ProfileCreateForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'bio', 'avatar')
+        fields = ('first_name', 'last_name', 'bio', 'avatar', 'activ_email',)
+        widgets = {'activ_email': forms.HiddenInput()}
