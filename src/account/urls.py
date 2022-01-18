c5a74db6b8e191bc800f37c7c779427c7764cd93
alex <martsenyk.l@gmail.com>
2022-01-18 16:28:04 +0200

    create function get_or_none

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path, include
 
+from src.account.forms import LoginForm
+from django.contrib.auth import views as auth_views
 from src.account.views import index, RegistrationUser, ProfileEdit, ToRegistrationUserEmail
 
 urlpatterns = [
     path('', index, name='home'),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Шаблоны для доступа к обработчикам смены пароля
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # Обработчики восстановления пароля
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('register/', RegistrationUser.as_view(), name='register'),
     path('register-to-email/', ToRegistrationUserEmail.as_view(), name='register_to_email'),
