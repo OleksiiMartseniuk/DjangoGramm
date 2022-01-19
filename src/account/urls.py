cb8fbf52dbbf1c657e13ce718adc678b38a60015
alex <martsenyk.l@gmail.com>
2022-01-19 21:18:56 +0200

    add path post -> detail

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from src.account.views import index, ProfileListView
+from src.account.views import index, ProfileListView, ImageDetailView
 
 urlpatterns = [
     path('', index, name='home'),
-    path('profile/<slug:username>/', ProfileListView.as_view(), name='profile')
+    path('profile/<slug:username>/', ProfileListView.as_view(), name='profile'),
+    path('image/<slug:slug>/', ImageDetailView.as_view(), name='detail_image'),
 ]
