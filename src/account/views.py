4ab36bd0744eb3418a907ab4427eaf14f9381549
alex <martsenyk.l@gmail.com>
2022-01-19 21:21:09 +0200

    crated get_context_data and class ImageDetailView

--- src/account/views.py
+++ src/account/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
 
+from src.account.forms import CommentForm
+from src.account.models import Post, Comment
 from src.authorization.models import CustomUser
 
 
@@ -20,3 +24,38 @@ class ProfileListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return self.request.user.posts.filter(status='published').all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        kwargs['section'] = 'profile'
+        return super(ProfileListView, self).get_context_data(**kwargs)
+
+
+class ImageDetailView(DetailView, FormMixin):
+    model = Post
+    template_name = 'account/profile/image_detail.html'
+    form_class = CommentForm
+
+    def get_success_url(self):
+        return reverse('detail_image', kwargs={'slug': self.object.slug})
+
+    def get_context_data(self, **kwargs):
+        kwargs['comments_list'] = Comment.objects.filter(post=self.object, active=True).all()
+        return super(ImageDetailView, self).get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['owner'] = self.request.user
+        initial['post'] = self.object
+        return initial
+
+    def form_valid(self, form):
+        form.save()
+        return super(ImageDetailView, self).form_valid(form)
