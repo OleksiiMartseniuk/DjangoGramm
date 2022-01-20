c967a74d2bcb4496d0f712131e86c7aba5d6e22e
alex <martsenyk.l@gmail.com>
2022-01-20 16:25:57 +0200

    add class PostCreateForm

--- src/account/forms.py
+++ src/account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from src.account.models import Comment
+from src.account.models import Comment, Post
 
 
 class CommentForm(forms.ModelForm):
@@ -10,4 +10,13 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'owner': forms.HiddenInput(),
             'post': forms.HiddenInput(),
+        }
+
+
+class PostCreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'image', 'description', 'owner')
+        widgets = {
+            'owner': forms.HiddenInput(),
         }
