c822664fbb9e3d82f2b0df3cfb57b2262e553326
alex <martsenyk.l@gmail.com>
2022-01-22 22:44:22 +0200

    edit url(profile_email)

--- src/authorization/urls.py
+++ src/authorization/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
 
     path('register/', RegistrationUser.as_view(), name='register'),
     path('register-to-email/', ToRegistrationUserEmail.as_view(), name='register_to_email'),
-    path('register/email/<uidb64>', ProfileEdit.as_view(), name='profile_edit'),
+    path('register/email/<uidb64>', ProfileEdit.as_view(), name='profile_email'),
 
 ]
