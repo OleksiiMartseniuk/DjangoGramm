19e3b22c9c9de18543d58ce2062bfc160e86e6fc
alex <martsenyk.l@gmail.com>
2022-01-25 23:23:02 +0200

    add comments

--- src/account/urls.py
+++ src/account/urls.py
@@ -3,14 +3,18 @@ from django.urls import path
 from src.account import views
 
 urlpatterns = [
+    # Home
     path('', views.index, name='home'),
+    # Profile
     path('profile/subscription/', views.SubscriptionHandler.as_view(), name='subscription'),
     path('profile/subscription/delete/<int:user_id>/', views.FollowersHandler.as_view(), name='subscription_delete'),
     path('profile/<slug:username>/', views.ProfileListView.as_view(), name='profile'),
     path('profile/edit/<slug:username>/', views.EditProfileUpdateView.as_view(), name='profile_edit'),
+    # Image
     path('image/like/', views.LikeImageHandler.as_view(), name='like'),
     path('image/create/', views.PostCreateView.as_view(), name='create_image'),
     path('image/<slug:slug>/', views.ImageDetailView.as_view(), name='detail_image'),
-    path('delete/image/<int:pk>/', views.PostDeleteView.as_view(), name='delete_image'),
-    path('delete/comment/<int:pk>/', views.CommentDeleteView.as_view(), name='delete_comment'),
+    path('image/delete/<int:pk>/', views.PostDeleteView.as_view(), name='delete_image'),
+    # Comment
+    path('comment/delete/<int:pk>/', views.CommentDeleteView.as_view(), name='delete_comment'),
 ]
--- src/authorization/urls.py
+++ src/authorization/urls.py
@@ -5,19 +5,21 @@ from django.contrib.auth import views as auth_views
 from src.authorization.views import RegistrationUser, ProfileEdit, ToRegistrationUserEmail
 
 urlpatterns = [
+    # Authentication
     path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    # Шаблоны для доступа к обработчикам смены пароля
+    # Templates for accessing password change handlers
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
-    # Обработчики восстановления пароля
+    # Password recovery handlers
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
+    # Register
     path('register/', RegistrationUser.as_view(), name='register'),
     path('register-to-email/', ToRegistrationUserEmail.as_view(), name='register_to_email'),
     path('register/email/<uidb64>', ProfileEdit.as_view(), name='profile_email'),
