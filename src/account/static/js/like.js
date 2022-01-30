b56156aeca7f992dfae5149f2e0b69f7b59b7d2a
alex <martsenyk.l@gmail.com>
2022-01-30 10:47:34 +0200

    edit js like

--- src/account/static/js/like.js
+++ src/account/static/js/like.js
@@ -1,15 +1,15 @@
    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
-   const like = document.getElementById('like')
-   like.onclick = () => {
-       let id = like.attributes[1].textContent
-       let action = like.attributes[2].textContent
-       let total = like.attributes[3].textContent
-       data = {
-         id: id,
-         action: action,
-       }
-       fetch("/image/like/", {
+   document.body.addEventListener("click", function(event){
+    if (event.target.id == "like"){
+      let id = event.target.attributes[1].textContent
+      let action = event.target.attributes[2].textContent
+      let total = event.target.attributes[3].textContent
+      data = {
+        id: id,
+        action: action,
+      }
+      fetch("/image/like/", {
         method: 'POST',
         body: JSON.stringify(data),
         headers: { 
@@ -18,20 +18,21 @@
         })
        .then(response => response.json())
        .then(data => {
-         if (data['status'] == 'ok'){
-            console.log(data)
-           if (action === 'like'){
-            like.attributes[2].textContent = 'unlike';    
-            like.classList.remove('btn-outline-secondary');
-            like.classList.add('btn-danger');
-            like.innerHTML = 'Like ' + (Number(total) + 1);
-           }
-           if (action === 'unlike'){
-            like.attributes[2].textContent = 'like';    
-            like.classList.remove('btn-danger');
-            like.classList.add('btn-outline-secondary');
-            like.innerHTML = 'Like ' + (Number(total) - 1);
-           }
-         }
-       });
-   };
+        if (data['status'] == 'ok'){
+           console.log(data)
+          if (action === 'like'){
+            event.target.attributes[2].textContent = 'unlike';    
+            event.target.classList.remove('btn-outline-secondary');
+            event.target.classList.add('btn-danger');
+            event.target.innerHTML = 'Like ' + (Number(total) + 1);
+          }
+          if (action === 'unlike'){
+            event.target.attributes[2].textContent = 'like';    
+            event.target.classList.remove('btn-danger');
+            event.target.classList.add('btn-outline-secondary');
+            event.target.innerHTML = 'Like ' + (Number(total) - 1);
+          }
+        }
+      });
+    }
+   });
