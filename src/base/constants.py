f1949b79a05e74891257a341515365e7cf0e4d59
alex <martsenyk.l@gmail.com>
2022-01-30 09:56:25 +0200

    create constants

---
+++ src/base/constants.py
@@ -0,0 +1,11 @@
+# authorization
+PROFILE_EDIT = 'Data saved'
+
+# account
+EDIT_PROFILE = 'Profile updated'
+POST_CREATE = 'Image add'
+
+# actions
+IMAGE = 'added a image'
+LIKES = 'likes'
+FOLLOWING = 'is following'
