4825128089b575f9e7c87ead0755eb364b284d76
alex <martsenyk.l@gmail.com>
2022-02-18 15:39:21 +0200

    DEBUG = False

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['damp-tor-88651.herokuapp.com', '127.0.0.1', 'localhost']
 
